a486b230dc17b654ade56f40dc2052ecad627f76
tangkong <tangkongrobert@gmail.com>
2024-05-03 10:40:28 -0700

    BLD: reference proper version in __init__.py

--- qtpyinheritance/__init__.py
+++ qtpyinheritance/__init__.py
@@ -1,6 +1,4 @@
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from .version import __version__  # noqa: F401
 
 from . import properties
 from . import signals
